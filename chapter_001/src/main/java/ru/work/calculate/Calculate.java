efc06be65399ec1ed45d3bc09d17b10f97884487
AlexIvts <alexivts@gmail.com>
2019-03-28 16:31:17 +0300

    CheckStyle

--- chapter_001/src/main/java/ru/work/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/work/calculate/Calculate.java
@@ -7,13 +7,13 @@ package ru.work.calculate;
 * @version 1
 
 */
-public class Calculate{
+public class Calculate {
 	
 	/**
 	* Конструктор, вывод строки в консоль.
 	* @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
 	
