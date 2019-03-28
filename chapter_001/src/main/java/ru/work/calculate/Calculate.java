a9a325d61af85d2a5d93407ba9fa2136ee999b40
AlexIvts <alexivts@gmail.com>
2019-03-28 12:30:25 +0300

    Maven

--- chapter_001/src/main/java/ru/work/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/work/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.work.calculate;
 
+/**
+* Class Класс для вычисления арифметических операций 
+* @author Alex Ivts
+* @since 28.03.2019
+* @vercion 1
+
+*/
 public class Calculate{
 	
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param name args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello");
 	}
