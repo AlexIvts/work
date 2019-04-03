be0123f4ce27d8411c2ea47c1ec863c6abf3a571
AlexIvts <alexivts@gmail.com>
2019-04-03 11:04:42 +0300

    Calculator 3.1

--- chapter_001/src/main/java/ru/work/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/work/calculate/Calculate.java
@@ -25,4 +25,44 @@ public class Calculate {
 	 public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	 }
+
+	 /**
+	  * Method add
+	  * @param first - first
+	  * @param second - second
+	  * @return first plus second
+	  */
+	 public double add(double first, double second) {
+	 	return first + second;
+	 }
+
+	/**
+	 * Method sub
+	 * @param first - first
+	 * @param second - second
+	 * @return first sub second
+	 */
+	public double sub(double first, double second) {
+		return first - second;
+	}
+
+	/**
+	 * Method mult
+	 * @param first - first
+	 * @param second - second
+	 * @return first mult second
+	 */
+	public double mult(double first, double second) {
+		return first * second;
+	}
+
+	/**
+	 * Method div
+	 * @param first - first
+	 * @param second - second
+	 * @return first div second
+	 */
+	public double div(double first, double second) {
+		return first / second;
+	}
 }
